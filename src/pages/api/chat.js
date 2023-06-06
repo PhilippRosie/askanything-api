acda33c72c2f86f35ac0e1c8daa93ffc42063181
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-06-06 02:29:04 +0200

    api

--- src/pages/api/chat.js
+++ src/pages/api/chat.js
@@ -1,20 +1,20 @@
 const { Configuration, OpenAIApi } = require("openai");
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.CHAT_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(req, res) {
   const { text } = req.query;
-  console.log(text);
+  console.log("4", text);
 
   const completion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: decodeURI(text),
+    prompt: text,
     max_tokens: 2048,
   });
-  console.log(completion.data.choices[0].text);
+  console.log("xi", completion.data.choices[0].text);
   res.status(200).json({ data: completion.data.choices[0].text });
 }
