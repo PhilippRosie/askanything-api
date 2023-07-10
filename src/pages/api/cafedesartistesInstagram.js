788aa7f45ea31630e72127e7fa66de6d1b22d021
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-07-11 00:45:45 +0200

    removed content in cafedesartistes

--- src/pages/api/cafedesartistesInstagram.js
+++ src/pages/api/cafedesartistesInstagram.js
@@ -1,9 +1,15 @@
-const fetch = require("node-fetch");
+import fetch from "isomorphic-unfetch";
 
 export default async function handler(req, res) {
-  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalinkm&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`;
-  const data = await fetch(url);
-  const feedData = await data.json();
+  const { REACT_APP_INSTAGRAM_TOKEN } = process.env;
 
-  res.status(200).json({ data: feedData });
+  try {
+    const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalinkm&access_token=${REACT_APP_INSTAGRAM_TOKEN}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
