58fb76fc8280c2a2d304122e0668c0c893ee9243
PhilippRosie <122967743+PhilippRosie@users.noreply.github.com>
2023-07-10 23:47:58 +0200

    change dthe instagram access

--- src/pages/api/cafedesartistesInstagram.js
+++ src/pages/api/cafedesartistesInstagram.js
@@ -1,3 +1,9 @@
-const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalinkm&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`;
-const data = await fetch(url);
-const feedData = await data.json();
+const fetch = require("node-fetch");
+
+export default async function handler(req, res) {
+  const url = `https://graph.instagram.com/me/media?fields=id,caption,media_url,timestamp,media_type,permalinkm&access_token=${process.env.REACT_APP_INSTAGRAM_TOKEN}`;
+  const data = await fetch(url);
+  const feedData = await data.json();
+
+  res.status(200).json({ data: feedData });
+}
